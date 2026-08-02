ef7c4e1982c178ce949db86f306dea9d67643487
Prathamesh Shinde <prathameshsshinde2006@email.com>
2026-08-02 13:25:38 +0530

    Display products from SQL Server using Entity Framework

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerceManagementSystem.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ECommerceManagementSystem.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public ProductController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return View(products);
         }
     }
 }
